3517d373c3af2703bbd6765b532cf4cac86b6ddc
pember0 <134245409+pember0@users.noreply.github.com>
2023-07-18 10:47:31 +0000

    商品の実装機能_20230718

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,4 @@
 class Item < ApplicationRecord
-  # items_imegeという名前でActiveStorageでプロフィール画像を保存できるように設定
+  # items_imegeという名前でActiveStorageで商品画像を保存できるように設定
   has_one_attached :item_image
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
 # 顧客用
-# URL /customers/sign_in...
-#  devise_for :customers,skip: [:passwords], controllers: {
-#    registrations: "public/registrations",
-#    sessions: 'public/sessions'
-#  }
- root to: "items#new"  # Topページを設定
+ #URL /customers/sign_in...
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+ root :to => "public/homes#top"  # Topページを設定
 # 管理者用
 # URL /admin/sign_in ...
-#  devise_for :admin,skip: [:registrations, :passwords], controllers: {
- #   sessions: "admin/sessions"
-#  }
+  devise_for :admin,skip: [:registrations, :passwords], controllers: {
+   sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 namespace :public do
@@ -20,6 +20,6 @@ end
 
 namespace :admin do
   resources :customers, only: [:index, :show, :edit, :update]
-  resources :items, only: [:new, :create, :index]
+  resources :items, only: [:new, :create, :index, :show, :edit, :update]
   end
 end
