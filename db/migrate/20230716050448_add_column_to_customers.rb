e47d16d08f6a98cc7997798cb18b9c65b24c82e7
kind2war <130664778+kind2war@users.noreply.github.com>
2023-07-19 07:33:32 +0000

    [add]layout

--- db/migrate/20230716050448_add_column_to_customers.rb
+++ db/migrate/20230716050448_add_column_to_customers.rb
@@ -2,6 +2,6 @@ class AddColumnToCustomers < ActiveRecord::Migration[6.1]
   def change
     add_column :customers, :family_name_kana, :string, null: false, default: ""
     add_column :customers, :first_name_kana, :string, null: false, default: ""
-    add_column :customers, :is_deleted, :boolean, null: false
+    add_column :customers, :is_deleted, :boolean, null: false, default: false
   end
 end
