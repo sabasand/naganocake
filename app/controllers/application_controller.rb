616ca4e2035f054e185766c1699925407fd65573
kind2war <130664778+kind2war@users.noreply.github.com>
2023-07-22 13:30:13 +0900

    application_controller.rb の更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,17 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: [:is_deleted])
   end
+
+private
+
+  # ログイン後のリダイレクト先
+  def after_sign_in_path_for(resource_or_scope)
+    admin_path #
+  end
+
+  # ログアウト後のリダイレクト先
+  def after_sign_out_path_for(resource_or_scope)
+    new_admin_session_path #ここを好きなパスに変更
+  end
+
 end
