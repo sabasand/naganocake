a1f0ea8538149ff986a8d77d218c136dd803f2d8
kind2war <130664778+kind2war@users.noreply.github.com>
2023-07-22 13:39:13 +0900

    [fix]devise_SignUp,SignOut
    権限を2つに分けたコントローラーでの、遷移先の変更。未テスト。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,15 +7,22 @@ class ApplicationController < ActionController::Base
   end
 
 private
+def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope.is_a?(Admin)
+        admins_orders_top_path
+    else
+        root_path
+    end
+end
 
-  # ログイン後のリダイレクト先
-  def after_sign_in_path_for(resource_or_scope)
-    admin_path #
-  end
-
-  # ログアウト後のリダイレクト先
-  def after_sign_out_path_for(resource_or_scope)
-    new_admin_session_path #ここを好きなパスに変更
-  end
+def
+after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :user
+        root_path
+    elsif resource_or_scope == :admin
+        new_admin_session_path
+    else
+        root_path
+    end
 
 end
