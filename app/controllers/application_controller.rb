c3372d96a87761067239295f50125a4cf132b298
kind2war <130664778+kind2war@users.noreply.github.com>
2023-07-23 11:57:15 +0900

    application_controller.rb の更新
    sign-out時のコードについて。:userとなっていた部分を、:customerに修正。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ end
 
 def
 after_sign_out_path_for(resource_or_scope)
-    if resource_or_scope == :user
+    if resource_or_scope == :customer
         root_path
     elsif resource_or_scope == :admin
         new_admin_session_path
