9759101b632e00bf01f20b5449baf3a593750966
shitiandazhi <135299404+shitiandazhi@users.noreply.github.com>
2023-07-25 04:44:44 +0000

    [fix]order-details

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -3,7 +3,7 @@ class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
 
-  enum production_status: { production_not_possible: 0, production_pending: 1, in_production: 2, production_complete: 3}
+  enum production_status: { 着手不可: 0, 製作待ち: 1, 製作中: 2, 製作完了: 3}
 
 
  def subtotal
