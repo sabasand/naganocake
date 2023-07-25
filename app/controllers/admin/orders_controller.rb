ff22f7e497632a27af1553d5f8dfeb9831323947
kind2war <130664778+kind2war@users.noreply.github.com>
2023-07-25 06:08:29 +0000

    [fix]admin/orders

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,13 +1,12 @@
 class Admin::OrdersController < ApplicationController
   def index
-    @order = Order.page(params[:page])
-    customer_id = Customer.find_by(id: @order.ids)
-    @full_name = customer_id.family_name + "" + customer_id.first_name
+    @orders = Order.page(params[:page])
   end
 
   def show
     @order = Order.find(params[:id])
     @order_details = OrderDetail.where(order_id: @order.id)
+    @full_name = @order.customer.family_name + "" + @order.customer.first_name
   end
 
    def update
