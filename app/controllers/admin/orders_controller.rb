45ae3acf3493f0809e1319fcd82dc1e1f8989684
kind2war <130664778+kind2war@users.noreply.github.com>
2023-07-25 01:02:21 +0000

    [add]管理者側,注文履歴詳細の購入者表示と遷移

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,6 +6,7 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
     @order_details = OrderDetail.all
+    @full_name = @order.customer.family_name + "" + @order.customer.first_name
   end
 
 end
